7849201c11281ea967fec22daa9904895203cd0d
Víctor Peñaló <victor.alexander23@gmail.com>
2019-04-29 19:35:20 -0400

    fix several comments

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ GITEMAIL="victor.alexander23@gmail.com"
 # Update system
 dnf update -y
 
- Set hostname
+# Set hostname
 hostnamectl set-hostname $USERNAME-pc
 
 # Install graphical enviroment
@@ -51,38 +51,38 @@ dnf install nautilus terminator timeshift firefox keepassxc libreoffice evince g
 dnf install inkscape kicad freecad -y
 dnf install gnome-boxes evolution kde-connect nextcloud-client neovim python{2,3}-neovim -y
 
-### Install i3-gaps
+# Install i3-gaps
 cd /tmp
 wget https://github.com/intart-do/rpmbuild/raw/master/RPMS/i3-gaps-4.16.1-1.fc29.x86_64.rpm
 dnf install i3-gaps-4.16.1-1.fc29.x86_64.rpm -y
 dnf install rofi i3status i3lock dunst compton feh -y
 
-### Install polybar
+# Install polybar
 cd /tmp
 wget https://github.com/intart-do/rpmbuild/raw/master/RPMS/polybar-3.3.1-1.x86_64.rpm
 dnf install polybar-3.3.1-1.x86_64.rpm -y
 
-### Install Spotify
+# Install Spotify
 
-### Install zsh
+# Install zsh
 dnf install zsh -y
 
-#### oh-my-zsh
+# oh-my-zsh
 sudo -u $USERNAME git clone https://github.com/robbyrussell/oh-my-zsh.git /home/$USERNAME/.oh-my-zsh
 
-#### zsh-autosuggestions
+# zsh-autosuggestions
 sudo -u $USERNAME git clone https://github.com/zsh-users/zsh-autosuggestions /home/$USERNAME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-#### zsh-completions 
+# zsh-completions 
 sudo -u $USERNAME git clone https://github.com/zsh-users/zsh-completions /home/$USERNAME/.oh-my-zsh/custom/plugins/zsh-completions
 
-#### zsh-syntax-highlighting
+# zsh-syntax-highlighting
 sudo -u $USERNAME git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/$USERNAME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-#### zsh-history-substring-search
+# zsh-history-substring-search
 sudo -u $USERNAME git clone https://github.com/zsh-users/zsh-history-substring-search /home/$USERNAME/.oh-my-zsh/custom/plugins/zsh-history-substring-search
 
-### Fantasque Fonts
+# Fantasque Fonts
 cd /tmp
 wget https://github.com/belluzj/fantasque-sans/releases/download/v1.7.2/FantasqueSansMono-Normal.zip
 unzip -o FantasqueSansMono-Normal.zip
@@ -90,7 +90,7 @@ cd OTF
 mkdir /usr/share/fonts/fantasque
 cp *.otf /usr/share/fonts/fantasque
 
-### Material Design Fonts
+# Material Design Fonts
 cd /tmp
 wget -O master.zip https://codeload.github.com/Templarian/MaterialDesign-Webfont/zip/master
 unzip -o master.zip
@@ -99,7 +99,7 @@ rm -R /usr/share/fonts/material-design
 mkdir /usr/share/fonts/material-design
 cp *.ttf /usr/share/fonts/material-design
 
-### Awesome For Desktop Fonts 
+# Awesome For Desktop Fonts 
 cd /tmp
 wget https://use.fontawesome.com/releases/v5.8.1/fontawesome-free-5.8.1-desktop.zip
 unzip -o fontawesome-free-5.8.1-desktop.zip
@@ -108,7 +108,7 @@ mkdir /usr/share/fonts/awesome
 cd fontawesome-free-5.8.1-desktop/otfs 
 cp *.otf /usr/share/fonts/awesome
 
-### Fira Mono Nerd Fonts
+# Fira Mono Nerd Fonts
 cd tmp
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/FiraMono.zip
 unzip -o FiraMono.zip 
@@ -117,10 +117,10 @@ mkdir /usr/share/fonts/fira-mono
 cp Fura\ Mono\ * /usr/share/fonts/fira-mono
 fc-cache -f
 
-### Create default user folders
+# Create default user folders
 sudo -u $USERNAME xdg-user-dirs-update
 
-### Change shell
+# Change shell
 sudo -u $USERNAME chsh -s $(which zsh)
 
 echo 'The End'
