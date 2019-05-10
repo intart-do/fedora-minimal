240630939765d95be44a1c8033fcce6fe31ceea6
Víctor Peñaló <victor.alexander23@gmail.com>
2019-05-10 15:28:33 -0400

    added neovim and zsh to stow

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ sudo -u $USERNAME git config --global user.email  $GITEMAIL
 # Set configuration files
 sudo -u $USERNAME git clone https://github.com/intart-do/.dotfiles.git /home/$USERNAME/.dotfiles
 cd /home/$USERNAME/.dotfiles
-sudo -u $USERNAME stow dunst i3 polybar rofi 
+sudo -u $USERNAME stow dunst i3 neovim polybar rofi zsh 
 
 # Install some utilities
 dnf install wget curl stow mlocate zip unzip tar wireless-tools crda network-manager-applet \
