c26187b5c745b105ab136e71ee0ea834def1f879
intart-do <victor.alexander23@gmail.com>
2019-09-08 11:55:27 -0400

    add some pulseaudio utilities

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-rel
 dnf install openssh-askpass gnome-keyring-pam gnome-keyring libgnome-keyring polkit polkit-gnome libsecret \
 gvfs gvfs-mtp gvfs-nfs gvfs-fuse gvfs-smb ntfs-3g \
 pulseaudio pulseaudio-esound-compat alsa-plugins-pulseaudio pulseaudio-module-x11 pulseaudio-module-gconf pulseaudio-utils \
-pavucontrol dejavu-sans-mono-fonts liberation-*-fonts \
+pavucontrol pasystray playerctl dejavu-sans-mono-fonts liberation-*-fonts \
 lxappearance qt5ct adwaita-qt4 adwaita-qt5 xdg-user-dirs \
 gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free \
 gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free-extras ffmpeg -y
